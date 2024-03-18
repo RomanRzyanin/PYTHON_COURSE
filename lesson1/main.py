e9c4076f4a76af3b5d1c4121675278f5a953f0bf
Roman Rzyanin <roman@rzyanin.ru>
2024-03-18 16:52:05 +0300

    Python_course

---
+++ lesson1/main.py
@@ -0,0 +1,101 @@
+#print(5, 8, 6)
+
+# n = 1.89
+# print(n)
+
+# n = 'fddf'
+# print(n)
+# n1 = "gwegf"
+
+# n = 'df\"d\"f'
+# a = 5
+# b = 3.65
+# c = 'hello'
+# print(a, '-', b, '-', c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}". format(a, b, c))
+
+# Ввод данных
+# print('Введите число:')
+# a = int(input())
+# print(f"a = {a}")
+
+# b = int(input ('Введите второе число: '))
+
+# print(a, '+', b, '=', a + b)
+
+# c = 1
+
+# print(type(c), c)
+
+# c = bool(c)
+# print(type(c), c)
+
+
+# a = 5.9074
+# b = 8.39476
+# print(round(a*b, 2))
+
+# iter = 2
+# iter += 3
+
+# a = 1 < 5 < 7 < 11
+# print(a)
+
+# n = 423
+# sum = 0
+# while n > 0:
+#     x = n % 10
+#     sum = sum + x
+#     n = n // 10
+# print(sum)    
+
+# i = 0
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i += 1
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag == False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+
+# for i in 1, -2, 3, 14, 5:
+#     print(i)
+
+# for i in range(100, 0, -20):
+#     print(i)
+
+# a = 'qwerty'
+
+# for i in a:
+#     print(i)
+
+# line = ''
+# for i in range(5):
+#     line = ''
+#     for j in range(5):
+#         line += '*'
+#     print(line)    
+
+text = 'СЪЕШь еще этиХ мягких ФранЦУЗКИХ булочек'
+
+print(text[: : 2])
+# print(len(text))
+# print('еще' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('еще', 'Исчо'))
