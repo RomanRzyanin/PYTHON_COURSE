2592ae567e5e3660141c0a95a3bf27534cf6977c
Roman Rzyanin <roman@rzyanin.ru>
2024-03-21 22:06:56 +0300

    Python_course_seminar_2

--- lesson1/massive.py
+++ lesson1/massive.py
@@ -1,17 +1,63 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
-motorcycles.append('ducati')
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# motorcycles.append('ducati')
 
-# i = 0
-# while i < 4:
-#     print(f'\t{motorcycles[i]}')
-#     i += 1
-#n = len(motorcycles)
-for i in motorcycles: #range(n):
+# # i = 0
+# # while i < 4:
+# #     print(f'\t{motorcycles[i]}')
+# #     i += 1
+# #n = len(motorcycles)
+# for i in motorcycles: #range(n):
     
-    print(f'\t{i}')
+#     print(f'\t{i}')
 
-#motorcycles_1 = motorcycles.pop(2)
-# motorcycles_2 = motorcycles.remove('yamaha')
-#print(motorcycles_2)    
-motorcycles.sort(reverse = True)
-print(motorcycles)
+# #motorcycles_1 = motorcycles.pop(2)
+# # motorcycles_2 = motorcycles.remove('yamaha')
+# #print(motorcycles_2)    
+# motorcycles.sort(reverse = True)
+# print(motorcycles)
+
+
+# array = []
+# for i in range(1234567):
+#     array.append(i)
+#     #    print(array[i])
+# print(min(array))
+# print(max(array))
+# print(sum(array))
+
+#print(array)
+
+
+# squares = []
+# for value in range(2, 11, 2):
+#     square = 2 ** value
+#     squares.append(square)
+# print(squares)
+
+# array = []
+# res = 1
+# for i in range(1, 11):
+#     res *= 2
+#     array.append(res)
+# print(array)
+
+# arr = [j ** 2 for j in range(1, 11)]
+# print(arr)
+
+# arr_3 = [k ** 3 for k in range(1, 11)]
+# print(arr_3)
+# print(arr_3[-2:])
+# for i in arr_3[-3:]:
+#     print(i)
+# arr_copy = arr_3[:]
+# arr_3.append('test')
+# print(arr_3)
+# print(arr_copy)
+# print(arr_3[10])
+
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+print(dimensions)
+dimensions = (400, 500)
+print(dimensions)
--- seminar2/main.py
+++ seminar2/main.py
@@ -4,7 +4,7 @@
 #     fact = 1
 #     while x > 1:
 #         fact = fact * x
-#         x = x - 1
+#         x -= 1
 #     return fact
         
 # n = int(input('Введите любое целое неотрицательное число: '))
@@ -12,23 +12,60 @@
 
 #task11
  
-# a = int(input('Введите любое целое неотрицательное число > 1: '))
+a = int(input('Введите любое целое неотрицательное число > 1: '))
 
-# if a == 0:  print(1)
+if a == 0:  print(1)
 
-# else:
+else:
 
-#     fib_prev, fib_next = 0, 1
-#     n = 1
+    fib_prev, fib_next = 0, 1
+    n = 1
 
-#     while fib_next <= a:
+    while fib_next <= a:
 
-#         if fib_next == a:
-#             print(n + 1)
-#             break
+        if fib_next == a:
+            print(n + 1)
+            break
 
-#         fib_prev, fib_next = fib_next, fib_prev + fib_next
-#         n += 1
-#     else:  print(-1)
+        # fib_prev, fib_next = fib_next, fib_prev + fib_next
+        temp = fib_next
+        fib_next = fib_prev + fib_next
+        fib_prev = temp
+        
+        n += 1
+    else:  print(-1)
     
 #task13
+# import random
+# n = int(input('Введите количество рассматриваемых дней N: \n'))
+# temp = []
+# for i in range (n):
+#     x = random.randint(-50, 50)
+#     temp.append(x)
+# print(f'Среднесуточная температура в соответсвующий день: {temp}\n')
+
+# j = 0
+# arr = []
+# #m = len(temp)
+# for i in range(len(temp)):
+#     if temp[i] > 0:
+#         j += 1
+    
+#     elif temp[i] <= 0: 
+#         arr.append(j)
+#         j = 0
+# print(arr)
+
+# t_max = 0
+# for i in range(len(arr)):
+#     if arr[i] > t_max:
+#         t_max = arr[i]
+        
+# if t_max == 1:
+#     print(f'Оттепель за период {n} дней, длилась {t_max} день')
+           
+# elif t_max == 2 or t_max == 3 or t_max == 4:       
+#     print(f'Оттепель за период {n} дней, длилась {t_max} дня')
+    
+# else:
+#     print(f'Оттепель за период {n} дней, длилась {t_max} дней')
